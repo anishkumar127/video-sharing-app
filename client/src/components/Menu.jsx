499ac0cdb9f8ce4ee9bc4f872c41c9a571a3ca3c
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-08 18:29:09 +0530

    package added

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import youtubeLogo from '../img/logo.png';
+import HomeIcon from '@mui/icons-material/Home';
 
 const Container = styled.div`
 flex:1;
 background-color:#202020;
 height:100vh;
 color:white;
+font-size:14px;
 `
 const Wrapper = styled.div`
 padding:18px ,26px;
 `
-const logo = styled.div`
+const Logo = styled.div`
 display:flex;
 align-items:center;
 gap:5px;
@@ -21,16 +23,26 @@ margin-bottom:25px;
 const Img = styled.img`
 height:25px;
 `
+const Item = styled.div`
+display:flex;
+align-items:center;
+gap:20px;
+cursor:pointer;
 
+`
 const Menu = () => {
   return (
     <Container>
       <Wrapper>
-        <logo>
-        <Img src={youtubeLogo}/>
-        youtube
-        </logo>
-        </Wrapper>
+        <Logo>
+          <Img src={youtubeLogo} />
+          youtube
+        </Logo>
+        <Item>
+          <HomeIcon/>
+          Home
+        </Item>
+      </Wrapper>
     </Container>
   )
 }
