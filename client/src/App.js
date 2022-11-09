d862d73def13702936ebdc7320f03657e80b7049
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-09 22:09:14 +0530

    route added home and video

--- client/src/App.js
+++ client/src/App.js
@@ -3,77 +3,48 @@ import styled, { ThemeProvider } from "styled-components";
 import Menu from './components/Menu';
 import Navbar from "./components/Navbar";
 import { darkTheme, lightTheme } from "./utils/Theme";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+// import Home from "@mui/icons-material/Home";
+import Home from './pages/Home';
+import Video from './pages/Video';
 const Container = styled.div`
 display:flex;
-`;
+`
+
 const Main = styled.div`
 flex:7;
 background-color:${({theme})=>theme.bg};
-`;
+`
+
 const Wrapper = styled.div`
-padding: 22px 96px;
-`;
+padding:22px 96px
+`
+
 function App() {
   const [darkMode,setDarkMode] = useState(true);
   return (
     <ThemeProvider theme={darkMode? darkTheme:lightTheme}>
    <Container>
+   <BrowserRouter>
     <Menu darkMode={darkMode} setDarkMode={setDarkMode}/>
     <Main>
       <Navbar/>
       <Wrapper>
-        video cards
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-        dddddddddsdfasd
-
+      <Routes>
+      <Route path="/">
+      <Route index element={<Home/>}/>
+      <Route path="video">
+      <Route path=":id" element={<Video/>}/>
+      </Route>
+      </Route>
+      </Routes>
       </Wrapper>
     </Main>
+    </BrowserRouter>
    </Container>
    </ThemeProvider>
   );
