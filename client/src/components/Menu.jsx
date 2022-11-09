87aae62c9fa705a6a6950909afe2d3af4ea46589
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-09 23:21:21 +0530

    routing added

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -17,6 +17,7 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
+import { Link } from 'react-router-dom';
 const Container = styled.div`
 flex:1;
 background-color:${({theme})=>theme.bgLighter};
@@ -75,10 +76,12 @@ const Menu = ({darkMode,setDarkMode}) => {
   return (
     <Container>
       <Wrapper>
-        <Logo>
+    <Link to="/" style={{textDecoration:"none",color:"inherit"}}>
+      <Logo>
           <Img src={youtubeLogo} />
-          youtube
+          YoutTube IN
         </Logo>
+    </Link>
         <Item>
           <HomeIcon/>
           Home
