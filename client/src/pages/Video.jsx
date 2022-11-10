1995c598f5119cebb100428b2261b60ab851cb12
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-10 17:48:48 +0530

    fixing video ui

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -38,11 +38,54 @@ const Buttons = styled.div`
   color: ${({ theme }) => theme.text};
 `;
 
-const Button = styled.div``;
-
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
 const Recommendation = styled.div`
   flex: 2;
 `;
+const Channel = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ChannelInfo = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Image = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const ChannelDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.text};
+`;
+
+const ChannelName = styled.span`
+  font-weight: 500;
+`;
+
+const ChannelCounter = styled.span`
+  margin-top: 5px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.textSoft};
+  font-size: 12px;
+`;
+
+const Description = styled.p``;
+
+const Subscribe = styled.button``;
 const Video = () => {
   return (
     <Container>
@@ -76,6 +119,26 @@ const Video = () => {
             </Button>
           </Buttons>
         </Details>
+        <Hr />
+        <Channel>
+          <ChannelInfo>
+            <Image src="https://i.ytimg.com/an_webp/FztSFtwobtM/mqdefault_6s.webp?du=3000&sqp=CKWFr5sG&rs=AOn4CLAMzCAbAKTIX--mtt7cbzIVVoFO6A" />
+
+            <ChannelDetail>
+              <ChannelName>Anish Kumar</ChannelName>
+              <ChannelCounter>400k subscribers</ChannelCounter>
+              <Description>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
+                consequatur enim ullam beatae illum sapiente exercitationem eos,
+                mollitia esse vitae blanditiis culpa quo illo at aspernatur quam
+                voluptates aliquam optio quis totam perferendis. Ipsum, facere
+                consectetur dolor totam assumenda asperiores dolore quaerat? Eos
+                enim aperiam dicta sequi unde laborum illo.
+              </Description>
+            </ChannelDetail>
+          </ChannelInfo>
+          <Subscribe>SUBSCRIBE</Subscribe>
+        </Channel>
       </Content>
       <Recommendation>recommendation</Recommendation>
     </Container>
