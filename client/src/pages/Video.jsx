2506fb048a883c008943038419c18cd73ac1bba5
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-10 17:27:10 +0530

    video player ui added

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOffAltOutlinedIcon from "@mui/icons-material/ThumbDownOffAltOutlined";
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
 
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+const Content = styled.div`
+  flex: 5;
+`;
+const VideoWrapper = styled.div``;
+
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled.span`
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 20px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Button = styled.div``;
+
+const Recommendation = styled.div`
+  flex: 2;
+`;
 const Video = () => {
   return (
-    <div>Video</div>
-  )
-}
+    <Container>
+      <Content>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="720"
+            src="https://www.youtube.com/embed/FztSFtwobtM"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </VideoWrapper>
+        <Title>Test Video</Title>
+        <Details>
+          <Info>3,323,432 views • Nov 10, 2022</Info>
+          <Buttons>
+            <Button>
+              <ThumbUpOutlinedIcon /> 123
+            </Button>
+            <Button>
+              <ThumbDownOffAltOutlinedIcon /> Dislike
+            </Button>
+            <Button>
+              <ReplyOutlinedIcon /> Share
+            </Button>
+            <Button>
+              <AddTaskOutlinedIcon /> Save
+            </Button>
+          </Buttons>
+        </Details>
+      </Content>
+      <Recommendation>recommendation</Recommendation>
+    </Container>
+  );
+};
 
-export default Video
+export default Video;
