2b5165afba6bdef602ada67d3e6532ac58542859
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2022-11-15 10:46:14 +0530

    Revert "server testing"

--- server/index.js
+++ server/index.js
@@ -1,19 +1,1 @@
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-const app = express();
-dotenv.config();
-
-// connection database
-const connectDB = ()=>{
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
-        console.group("Connected to Database");
-    }).catch((err)=>{
-        throw err;
-    })
-}
-
-app.listen(8800,()=>{
-    connectDB();
-    console.log("Connected to server");
-})
