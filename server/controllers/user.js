f5b57c5739d425709a24b0e9b8ac1f947f71462c
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-15 15:46:38 +0530

    subscribe and unsubscribe routing aded

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -31,8 +31,49 @@ export const deleteUser = async (req, res, next) => {
     return next(createError(403, "You can delete only your account!"));
   }
 };
-export const getUser = async (req, res, next) => {};
-export const subscribe = async (req, res, next) => {};
-export const unsubscribe = async (req, res, next) => {};
-export const like = async (req, res, next) => {};
-export const dislike = async (req, res, next) => {};
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+export const subscribe = async (req, res, next) => {
+  try {
+    await User.findById(req.user.id, {
+      $push: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 },
+    });
+    res.status(200).json("Subscription Successfull.");
+  } catch (err) {
+    next(err);
+  }
+};
+export const unsubscribe = async (req, res, next) => {
+  try {
+    await User.findById(req.user.id, {
+      $pull: { subscribedUsers: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: -1 },
+    });
+    res.status(200).json("Unsubscription Successfull.");
+  } catch (err) {
+    next(err);
+  }
+};
+export const like = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+export const dislike = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
