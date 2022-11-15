48cffb6b96261fc91081ffbe19ca3f926f976d3f
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-15 17:40:47 +0530

    search by tag and subs routing done

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,5 +1,6 @@
 import { createError } from "../error.js";
 import Video from "../models/Video.js";
+import User from "../models/User.js";
 
 export const addVideo = async (req, res, next) => {
   const newVideo = new Video({ userId: req.user.id, ...req.body });
@@ -65,24 +66,47 @@ export const addView = async (req, res, next) => {
 };
 export const trend = async (req, res, next) => {
   try {
-    const video = await Video.findById(req.params.id);
-    res.status(200).json(video);
+    const videos = await Video.find().sort({ views: -1 });
+    res.status(200).json(videos);
   } catch (err) {
     next(err);
   }
 };
 export const random = async (req, res, next) => {
   try {
-    const video = await Video.findById(req.params.id);
-    res.status(200).json(video);
+    const videos = await Video.aggregate([{ $sample: { size: 40 } }]);
+    res.status(200).json(videos);
   } catch (err) {
     next(err);
   }
 };
 export const sub = async (req, res, next) => {
   try {
-    const video = await Video.findById(req.params.id);
-    res.status(200).json(video);
+    const user = await User.findById(req.user.id);
+    const subscribedChannels = user.subscribedUsers;
+    const list = await Promise.all(
+      subscribedChannels.map((channelId) => {
+        return Video.find({ userId: channelId });
+      })
+    );
+    res.status(200).json(list.flat().sort((a, b) => b.createdAt - a.createdAt));
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const search = async (req, res, next) => {
+  try {
+    const videos = await Video.find().sort({ views: -1 });
+    res.status(200).json(videos);
+  } catch (err) {
+    next(err);
+  }
+};
+export const getByTag = async (req, res, next) => {
+  try {
+    const videos = await Video.find().sort({ views: -1 });
+    res.status(200).json(videos);
   } catch (err) {
     next(err);
   }
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -3,8 +3,10 @@ import {
   addVideo,
   addView,
   deleteVideo,
+  getByTag,
   getVideo,
   random,
+  search,
   sub,
   trend,
   updateVideo,
@@ -21,6 +23,8 @@ router.get("/find/:id", getVideo);
 router.put("/view/:id", addView);
 router.get("/trend", trend);
 router.get("/random", random);
-router.get("/sub", sub);
+router.get("/sub", verifyToken, sub);
+router.get("/tags", getByTag);
+router.get("/search", search);
 
 export default router;
