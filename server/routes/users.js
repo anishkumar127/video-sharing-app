ef515bf6b3a6666040e81377e16f62d03c90c7e4
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-15 15:22:32 +0530

    update and delete route done

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,35 @@
-import express from 'express';
-import {  } from '../controllers/user.js';
+import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  subscribe,
+  unsubscribe,
+  like,
+  dislike,
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
+// update user
+router.put("/:id", verifyToken, updateUser);
+// delete user
+router.delete("/:id", verifyToken, deleteUser);
+
+// get a user
+router.get("/find/:id", getUser);
+
+// subscribe a user
+router.put("/sub/:id", verifyToken, subscribe);
+
+// unsubscribe a user
+router.put("/unsub/:id", verifyToken, unsubscribe);
+
+// like a video
+router.put("/like/:videoId", verifyToken, like);
+
+// dislike a video
+router.put("/dislike/:videoId", verifyToken, dislike);
 
 export default router;
