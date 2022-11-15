cb95ad262caba1a04c1742cec76a78ab8d908963
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-15 14:03:51 +0530

    jwt token features added

--- server/index.js
+++ server/index.js
@@ -5,37 +5,42 @@ import userRoutes from "./routes/users.js";
 import videoRoutes from "./routes/videos.js";
 import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+
 const app = express();
 dotenv.config();
 
 // connection database
-const connectDB = ()=>{
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
-        console.group("Connected to Database");
-    }).catch((err)=>{
-        throw err;
+const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.group("Connected to Database");
     })
+    .catch((err) => {
+      throw err;
+    });
 };
 
-
 // routes
+app.use(cookieParser());
 app.use(express.json());
-app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/videos",videoRoutes);
-app.use("/api/comments",commentRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
 // middleware
-app.use((err,req,res,next)=>{
-    const status = err.status || 500;
-    const message = err.message || "Something went wrong!";
-    return res.status(status).json({
-        success:false,
-        status,
-        message
-    });
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
 });
-app.listen(8800,()=>{
-    connectDB();
-    console.log("Connected to server");
-})
+app.listen(8800, () => {
+  connectDB();
+  console.log("Connected to server");
+});
