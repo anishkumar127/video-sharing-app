ee3c40ff1071258b1ffabb5286f5b30e9589210b
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-15 12:44:03 +0530

    fixed

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+const app = express();
+dotenv.config();
+
+// connection database
+const connectDB = ()=>{
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.group("Connected to Database");
+    }).catch((err)=>{
+        throw err;
+    })
+};
+
+
+// routes
+app.use("/api/users",userRoutes);
+
+app.listen(8800,()=>{
+    connectDB();
+    console.log("Connected to server");
+})
