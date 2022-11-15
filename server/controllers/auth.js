e637f67392a4dc710d764c675c733490f9b41018
Anish Kumar <anishbishnoi127@gmail.com>
2022-11-15 13:49:27 +0530

    adding Hashing

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
+import { createError } from '../error.js';
 export const signup = async(req , res,next)=>{
     try{
         const salt = bcrypt.genSaltSync(10);
@@ -8,6 +9,17 @@ export const signup = async(req , res,next)=>{
         const newUser = new User({...req.body,password:hash});
         await newUser.save();
       res.status(200).send("User has been created!");
+    }catch(err){
+        next(err);
+    }
+};
+
+export const signin = async(req , res,next)=>{
+    try{
+       const user = await User.findOne({name:req.body.name});
+       if(!user) return next(createError(404,"User not found!"));
+       const isCorrect = await bcrypt.compare(req.body.password,user.password);
+       if(!isCorrect) return next(createError(400,"Wrong Credentials!"));
     }catch(err){
         next(err);
     }
